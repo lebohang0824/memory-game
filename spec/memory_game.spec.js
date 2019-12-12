fdf24927302064f04511463441007bb48bc1b952
Lebohang <lebohang0824@gmail.com>
2019-12-12 15:54:28 +0200

    Change file structure

---
+++ spec/memory_game.spec.js
@@ -0,0 +1,14 @@
+const jsdom = require('jsdom')       
+const {JSDOM} = jsdom;
+
+describe('Memory game', function () {
+
+   beforeEach(() => {
+      const dom = new JSDOM(`<!DOCTYPE html><div class="container"></div>`);
+   });   
+
+   it('', function () {
+
+   });
+
+});
